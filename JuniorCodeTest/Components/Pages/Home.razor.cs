39c5d818ee4b8ae870dd85b84028a384a1846d12
Binisha <binisha@ibatechnologies.com>
2024-05-11 19:34:33 +0100

    Initial commit@

---
+++ JuniorCodeTest/Components/Pages/Home.razor.cs
@@ -0,0 +1,38 @@
+﻿using JuniorCodeTest.Interfaces;
+using JuniorCodeTest.Models;
+
+using Microsoft.AspNetCore.Components;
+
+
+namespace JuniorCodeTest.Components.Pages
+{
+	public partial class Home : ComponentBase
+	{
+		public List<RequestedUsersModel> RandomUsers { get; set; } = [];
+
+		[Inject]
+		private IRandomUserApiService? RandomUserApiService { get; set; }
+
+
+		
+
+		protected override async Task OnInitializedAsync()
+		{
+			await RefreshUsers();
+		}
+
+		private async Task RefreshUsers()
+		{
+			try
+			{
+				RandomUsers = await RandomUserApiService.GetRandomUserDataFromApi();
+				RandomUsers = RandomUsers.Take(5).ToList(); // Take only the first 5 random users
+			}
+			catch (Exception ex)
+			{
+				// Handle exception
+				Console.WriteLine(ex.Message);
+			}
+		}
+	}
+}
